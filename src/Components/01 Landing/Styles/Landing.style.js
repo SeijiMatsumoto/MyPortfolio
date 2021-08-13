2e585c77a052e542db91232b5889c20487aeed06
Seiji Matsumoto <seijim27@gmail.com>
2021-08-13 00:50:47 -0400

    Added responsiveness for landing

--- src/Components/01 Landing/Styles/Landing.style.js
+++ src/Components/01 Landing/Styles/Landing.style.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
+import breakpoint from '../../../breakpoints.js';
 const keyframes = require('styled-components').keyframes
+
+/*
+  @media only screen and ${breakpoint.device.xs} {
+
+  }
+  @media only screen and ${breakpoint.device.sm}{
+
+  }
+  @media only screen and ${breakpoint.device.lg}{
+
+  }
+*/
 
 export const LandingContainer = styled.div`
   position: relative;
@@ -33,8 +46,7 @@ export const Bg = styled.div`
 export const Terminal = styled.div`
   margin: 0;
   padding: 0;
-  width: 900px;
-  height: 600px;
+
   background-color: black;
   border-radius: 1%;
   position: relative;
@@ -42,6 +54,19 @@ export const Terminal = styled.div`
   align-items: center;
   justify-content: center;
   z-index: 1;
+
+  @media only screen and ${breakpoint.device.xs} {
+    width: 400px;
+    height: 250px;
+  }
+  @media only screen and ${breakpoint.device.sm}{
+    width: 700px;
+    height: 400px;
+  }
+  @media only screen and ${breakpoint.device.lg}{
+    width: 900px;
+    height: 600px;
+  }
 `;
 
 export const TextDiv = styled.div`
@@ -98,7 +123,6 @@ export const blink = keyframes`
 `;
 
 export const Hello = styled.p`
-  font-size: 40px;
   color: white;
   overflow: hidden;
   border-right: .5em solid #FF4C29;
@@ -111,33 +135,92 @@ export const Hello = styled.p`
   left: 0;
   right: 0;
 
-  &:nth-child(1) {
-    width: 375px;
-    opacity: 0;
-    animation: ${type} 0.5s steps(40, end);
-    animation-fill-mode: forwards;
-    animation-delay: 1s;
-    top: 200px;
-  };
+  @media only screen and ${breakpoint.device.xs} {
+    font-size: 20px;
+    &:nth-child(1) {
+      width: 185px;
+      opacity: 0;
+      animation: ${type} 0.5s steps(40, end);
+      animation-fill-mode: forwards;
+      animation-delay: 1s;
+      top: 90px;
+    };
 
-  &:nth-child(2) {
-    font-size: 80px;
-    width: 800px;
-    opacity: 0;
-    animation: ${type2} 0.5s steps(40, end);
-    animation-delay: 2s;
-    animation-fill-mode: forwards;
-    top: 240px;
-  };
+    &:nth-child(2) {
+      font-size: 30px;
+      width: 300px;
+      opacity: 0;
+      animation: ${type2} 0.5s steps(40, end);
+      animation-delay: 2s;
+      animation-fill-mode: forwards;
+      top: 120px;
+    };
 
-  &:nth-child(3) {
-    width: 555px;
-    opacity: 0;
-    animation: ${type3} 0.5s steps(40, end), ${blink} .5s step-end infinite alternate;
-    animation-delay: 4s;
-    animation-fill-mode: forwards;
-    bottom: 30px;
-    margin: auto;
+    &:nth-child(3) {
+      display: none;
+    };
   }
+
+  @media only screen and ${breakpoint.device.sm}{
+    &:nth-child(1) {
+      font-size: 30px;
+      width: 270px;
+      opacity: 0;
+      animation: ${type} 0.5s steps(40, end);
+      animation-fill-mode: forwards;
+      animation-delay: 1s;
+      top: 150px;
+    };
+
+    &:nth-child(2) {
+      font-size: 50px;
+      width: 500px;
+      opacity: 0;
+      animation: ${type2} 0.5s steps(40, end);
+      animation-delay: 2s;
+      animation-fill-mode: forwards;
+      top: 200px;
+    };
+
+    &:nth-child(3) {
+      display: none;
+    };
+  }
+
+  @media only screen and ${breakpoint.device.lg}{
+    &:nth-child(1) {
+      font-size: 40px;
+      width: 375px;
+      opacity: 0;
+      animation: ${type} 0.5s steps(40, end);
+      animation-fill-mode: forwards;
+      animation-delay: 1s;
+      top: 200px;
+    };
+
+    &:nth-child(2) {
+      font-size: 80px;
+      width: 800px;
+      opacity: 0;
+      animation: ${type2} 0.5s steps(40, end);
+      animation-delay: 2s;
+      animation-fill-mode: forwards;
+      top: 250px;
+    };
+
+    &:nth-child(3) {
+      font-size: 40px;
+      width: 555px;
+      opacity: 0;
+      animation: ${type3} 0.5s steps(40, end), ${blink} .5s step-end infinite alternate;
+      animation-delay: 4s;
+      animation-fill-mode: forwards;
+      bottom: 30px;
+      margin: auto;
+      display: block;
+    };
+  }
+
+
 `;
 
