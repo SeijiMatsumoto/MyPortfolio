3021fa0fdd9b8e8dda2a0c3918d49bc3943c67fd
Seiji Matsumoto <seijim27@gmail.com>
2021-08-19 16:44:00 -0400

    Added more to stack

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,21 +3,21 @@ export const projects = [
     title: '01. Fashion E-commerce Site',
     url: 'https://i.imgur.com/JDyMBJH.png',
     description: 'Front-end focused e-commerce site built with a group with other engineers. Complete with a product overview section, related products section, and ratings/reviews. All data pull from a clothing store API.',
-    stack: ['React', 'Node.js', 'Styled Components'],
+    stack: ['Front-end', 'React', 'Node.js', 'Styled Components'],
     github: 'https://github.com/FEC8-Windham/fec'
   },
   {
     title: '02. Restaurant Website (WIP)',
     url: 'https://i.imgur.com/bv5z0Cu.png',
     description: 'Full-stack website created for a local ramen restaurant, Ramen Matsu, that is mobile-friendly and connects with the Square API that allows for placing orders through the website. The original website was created using Square\'s website builder, but was very limited in functionality and creative freedom. This website was created to solve those issues.',
-    stack: ['React', 'Node.js', 'Express'],
+    stack: ['Full-stack', 'React', 'Node.js', 'Express'],
     github: ''
   },
   {
     title: '03. Instagram Bot',
     url: 'https://i.imgur.com/mu0H6rX.png',
     description: 'A bot written in Python using Selenium, which is used to control things in a browser. This bot mimics real human interaction with other accounts and posts, with the goal of naturally gaining followers over time. The bot has increased my dog\'s Instagram following by over 1000 in just two months.',
-    stack: ['Python', 'Selenium'],
+    stack: ['Script', 'Python', 'Selenium'],
     github: 'https://github.com/SeijiMatsumoto/igbotv2_MacOS'
   }
 ]
