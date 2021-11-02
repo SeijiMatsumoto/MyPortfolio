f2d2859ca6160b622c08ae4d3934f0ffa67bbd7e
Seiji Matsumoto <seijim27@gmail.com>
2021-11-02 19:01:02 -0400

    Updated resume

--- src/Components/00 Navbar/Links.jsx
+++ src/Components/00 Navbar/Links.jsx
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { LinksUl, Link, A, Resume, ResumeDownload } from './Styles/Links.style'
-import anime from 'animejs';
+import React, { useEffect, useState } from "react";
+import { LinksUl, Link, A, Resume, ResumeDownload } from "./Styles/Links.style";
+import anime from "animejs";
 
 const Links = (props) => {
-
   var [showLinks, setLinks] = useState(true);
-  var [color, setColor] = useState('white');
+  var [color, setColor] = useState("white");
 
   useEffect(() => {
-    window.addEventListener('resize', displayLinks)
+    window.addEventListener("resize", displayLinks);
     displayLinks();
     animateOnLoad();
   }, []);
 
   useEffect(() => {
     if (!props.isDark) {
-      setColor('#1E242A');
+      setColor("#1E242A");
     } else {
-      setColor('white');
+      setColor("white");
     }
-  }, [props.isDark])
+  }, [props.isDark]);
 
   const displayLinks = () => {
     if (window.innerWidth > 1050) {
@@ -28,62 +27,115 @@ const Links = (props) => {
     } else {
       setLinks(false);
     }
-  }
+  };
 
   const animateOnLoad = () => {
-    const timeline = anime.timeline({
-      autoplay: false
-    })
-    .add({
-      targets: '#link1',
-      translateY: 25,
-      easing: 'spring',
-      opacity: [0, 1],
-      duration: 300,
-    }, '+=200')
-    .add({
-      targets: '#link2',
-      translateY: 25,
-      easing: 'spring',
-      opacity: [0, 1],
-      duration: 300,
-    }, '-=1500')
-    .add({
-      targets: '#link3',
-      translateY: 25,
-      easing: 'spring',
-      opacity: [0, 1],
-      duration: 300,
-    }, '-=1500')
-    .add({
-      targets: '#link4',
-      translateY: 25,
-      easing: 'spring',
-      opacity: [0, 1],
-      duration: 300,
-    }, '-=1500')
-    .add({
-      targets: '#resume',
-      translateY: 25,
-      easing: 'spring',
-      opacity: [0, 1],
-      duration: 300,
-    }, '-=1500')
+    const timeline = anime
+      .timeline({
+        autoplay: false,
+      })
+      .add(
+        {
+          targets: "#link1",
+          translateY: 25,
+          easing: "spring",
+          opacity: [0, 1],
+          duration: 300,
+        },
+        "+=200"
+      )
+      .add(
+        {
+          targets: "#link2",
+          translateY: 25,
+          easing: "spring",
+          opacity: [0, 1],
+          duration: 300,
+        },
+        "-=1500"
+      )
+      .add(
+        {
+          targets: "#link3",
+          translateY: 25,
+          easing: "spring",
+          opacity: [0, 1],
+          duration: 300,
+        },
+        "-=1500"
+      )
+      .add(
+        {
+          targets: "#link4",
+          translateY: 25,
+          easing: "spring",
+          opacity: [0, 1],
+          duration: 300,
+        },
+        "-=1500"
+      )
+      .add(
+        {
+          targets: "#resume",
+          translateY: 25,
+          easing: "spring",
+          opacity: [0, 1],
+          duration: 300,
+        },
+        "-=1500"
+      );
     timeline.play();
-  }
+  };
 
   return (
     <div>
-      {showLinks ?
-      <LinksUl>
-        <Link id='link1'>01.<A color={color} href='#about'>About</A></Link>
-        <Link id='link2'>02.<A color={color} href='#projects'>Projects</A></Link>
-        <Link id='link3'>03.<A color={color} href='#experience'>Experience</A></Link>
-        <Link id='link3'>04.<A color={color} href='#education'>Education</A></Link>
-        <Link id='link4'>05.<A color={color} href='#contact'>Contact</A></Link>
-        <Link id='resume'><Resume color={color}><ResumeDownload color={color} href='/FSSeijiMatsumotoResumeSept2021.pdf' download rel="noopener noreferrer" target='_blank'>Resume</ResumeDownload></Resume></Link>
-      </LinksUl>
-      : null }
+      {showLinks ? (
+        <LinksUl>
+          <Link id="link1">
+            01.
+            <A color={color} href="#about">
+              About
+            </A>
+          </Link>
+          <Link id="link2">
+            02.
+            <A color={color} href="#projects">
+              Projects
+            </A>
+          </Link>
+          <Link id="link3">
+            03.
+            <A color={color} href="#experience">
+              Experience
+            </A>
+          </Link>
+          <Link id="link3">
+            04.
+            <A color={color} href="#education">
+              Education
+            </A>
+          </Link>
+          <Link id="link4">
+            05.
+            <A color={color} href="#contact">
+              Contact
+            </A>
+          </Link>
+          <Link id="resume">
+            <Resume color={color}>
+              <ResumeDownload
+                color={color}
+                href="/SeijiMatsumotoResume2021.pdf"
+                download
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Resume
+              </ResumeDownload>
+            </Resume>
+          </Link>
+        </LinksUl>
+      ) : null}
     </div>
   );
 };
